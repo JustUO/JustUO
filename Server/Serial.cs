8c99d23de878c936df3800f627a4ef5da58d9f3f
CeJayce <cjcjdj@googlemail.com>
2015-05-05 16:42:48 +0100

    Serial.cs revert

--- Server/Serial.cs
+++ Server/Serial.cs
@@ -14,42 +14,30 @@
 
 #region References
 using System;
-using System.Globalization;
 #endregion
 
 namespace Server
 {
-	public struct Serial : IComparable, IComparable<Serial>, IEquatable<Serial>, IEquatable<Int32>
+	public struct Serial : IComparable, IComparable<Serial>
 	{
 		private readonly int m_Serial;
 
-		public const int MobileMin = 0x00000000;
-		public const int MobileMax = 0x3FFFFFFF;
-
-		public const int ItemMin = 0x40000000;
-		public const int ItemMax = 0x7FFFFFFE;
-		
-		public static readonly Serial MinusOne = new Serial(-1);
-		public static readonly Serial Zero = new Serial(0);
-
-		private static Serial m_LastMobile = MobileMin;
-		private static Serial m_LastItem = ItemMin;
+		private static Serial m_LastMobile = Zero;
+		private static Serial m_LastItem = 0x40000000;
 
 		public static Serial LastMobile { get { return m_LastMobile; } }
 		public static Serial LastItem { get { return m_LastItem; } }
+
+		public static readonly Serial MinusOne = new Serial(-1);
+		public static readonly Serial Zero = new Serial(0);
 
 		public static Serial NewMobile
 		{
 			get
 			{
-				int lastMobile = m_LastMobile;
-
-				while (m_LastMobile.m_Serial < MobileMax && ++lastMobile <= MobileMax)
+				while (World.FindMobile(m_LastMobile = (m_LastMobile + 1)) != null)
 				{
-					if (!World.Items.ContainsKey(m_LastMobile = lastMobile))
-					{
-						break;
-					}
+					;
 				}
 
 				return m_LastMobile;
@@ -60,14 +48,9 @@ namespace Server
 		{
 			get
 			{
-				int lastItem = m_LastItem;
-
-				while (m_LastItem.m_Serial < ItemMax && ++lastItem <= ItemMax)
+				while (World.FindItem(m_LastItem = (m_LastItem + 1)) != null)
 				{
-					if (!World.Items.ContainsKey(m_LastItem = lastItem))
-					{
-						break;
-					}
+					;
 				}
 
 				return m_LastItem;
@@ -81,11 +64,11 @@ namespace Server
 
 		public int Value { get { return m_Serial; } }
 
-		public bool IsMobile { get { return m_Serial > MobileMin && m_Serial <= MobileMax; } }
+		public bool IsMobile { get { return (m_Serial > 0 && m_Serial < 0x40000000); } }
 
-		public bool IsItem { get { return m_Serial >= ItemMin && m_Serial <= ItemMax; } }
+		public bool IsItem { get { return (m_Serial >= 0x40000000 && m_Serial <= 0x7FFFFFFF); } }
 
-		public bool IsValid { get { return m_Serial > 0; } }
+		public bool IsValid { get { return (m_Serial > 0); } }
 
 		public override int GetHashCode()
 		{
@@ -103,8 +86,7 @@ namespace Server
 			{
 				return CompareTo((Serial)other);
 			}
-			
-			if (other == null)
+			else if (other == null)
 			{
 				return -1;
 			}
@@ -112,19 +94,14 @@ namespace Server
 			throw new ArgumentException();
 		}
 
-		public bool Equals(Serial other)
-		{
-			return m_Serial == other.m_Serial;
-		}
-
-		public bool Equals(Int32 value)
-		{
-			return m_Serial == value;
-		}
-
 		public override bool Equals(object o)
 		{
-			return o is Serial && Equals((Serial)o);
+			if (o == null || !(o is Serial))
+			{
+				return false;
+			}
+
+			return ((Serial)o).m_Serial == m_Serial;
 		}
 
 		public static bool operator ==(Serial l, Serial r)
@@ -157,57 +134,24 @@ namespace Server
 			return l.m_Serial <= r.m_Serial;
 		}
 
+		/*public static Serial operator ++ ( Serial l )
+        {
+        return new Serial( l + 1 );
+        }*/
+
 		public override string ToString()
 		{
 			return String.Format("0x{0:X8}", m_Serial);
 		}
 
-		public static implicit operator int(Serial serial)
+		public static implicit operator int(Serial a)
 		{
-			return serial.m_Serial;
+			return a.m_Serial;
 		}
 
-		public static implicit operator Serial(int value)
+		public static implicit operator Serial(int a)
 		{
-			return new Serial(value);
-		}
-
-		public static bool TryParse(string value, out Serial serial)
-		{
-			int val;
-
-			if (Int32.TryParse(value, out val))
-			{
-				serial = val;
-				return true;
-			}
-
-			serial = MinusOne;
-			return false;
-		}
-
-		public static bool TryParse(string value, NumberStyles style, IFormatProvider format, out Serial serial)
-		{
-			int val;
-
-			if (Int32.TryParse(value, style, format, out val))
-			{
-				serial = val;
-				return true;
-			}
-
-			serial = MinusOne;
-			return false;
-		}
-
-		public static Serial Parse(string value)
-		{
-			return new Serial(Int32.Parse(value));
-		}
-
-		public static Serial Parse(string value, NumberStyles style)
-		{
-			return new Serial(Int32.Parse(value, style));
+			return new Serial(a);
 		}
 	}
 }
